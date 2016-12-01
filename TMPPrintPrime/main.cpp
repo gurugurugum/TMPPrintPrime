3d701ea1b1e2c0484dac37b76e226fbbe7b717db
gurugurugum <wrsqq331@gmail.com>
2016-12-02 00:56:41 +0900

    Initial Commit

---
+++ TMPPrintPrime/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TMPPrintPrime
+//
+//  Created by 岡 和磨 on 12/2/16.
+//  Copyright © 2016 岡 和磨. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
